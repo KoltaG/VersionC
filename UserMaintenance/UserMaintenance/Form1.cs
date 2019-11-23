6e972e12a4a8d3cc31cc387cb87c29bf6a92697b
Balázs Koltai <balazs.koltai@stud.uni-corvinus.hu>
2019-11-23 18:51:35 +0100

    filebairas

--- UserMaintenance/UserMaintenance/Form1.cs
+++ UserMaintenance/UserMaintenance/Form1.cs
@@ -3,6 +3,7 @@ using System.Collections.Generic;
 using System.ComponentModel;
 using System.Data;
 using System.Drawing;
+using System.IO;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -18,12 +19,14 @@ namespace UserMaintenance
         {
             InitializeComponent();
             label1.Text = Resource1.FullName; // label1
-            
+
             button1.Text = Resource1.Add; // button1
+            button2.Text = Resource1.Write;
 
             listBox1.DataSource = users;
             listBox1.ValueMember = "ID";
             listBox1.DisplayMember = "FullName";
+
         }
 
         private void button1_Click(object sender, EventArgs e)
@@ -31,9 +34,36 @@ namespace UserMaintenance
             var u = new User()
             {
                 FullName = textBox1.Text,
-             
+
             };
             users.Add(u);
         }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            SaveFileDialog sfd = new SaveFileDialog();
+            sfd.Filter = "TXT file (*.txt)|*.txt| All Files (*.*)|*.*";
+            sfd.Title = "Mentes";
+            sfd.ShowDialog();
+            if (sfd.FileName != "")
+            {
+                var emberek = (from x in users select x).ToList();
+
+                using (StreamWriter sw = new StreamWriter(sfd.FileName, true, Encoding.UTF8))
+                {
+                    sw.WriteLine("ID ; Teljes név");
+                    foreach (var item in users)
+                    {
+                        sw.WriteLine(item.ID + " " + item.FullName);
+                        
+                    }
+                   
+                    //devben elfelejtettem kitörölni a dobozt meg a labelt
+                }
+
+
+            }
+        }
+
     }
 }
